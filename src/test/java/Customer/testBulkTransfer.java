e66abe73042fca5e009b3b2a043d3360006cc5ab
Prajwal019 <129170313+Prajwal019@users.noreply.github.com>
2024-05-22 10:17:30 +0530

    Update testBulkTransfer.java

--- src/test/java/Customer/testBulkTransfer.java
+++ src/test/java/Customer/testBulkTransfer.java
@@ -74,4 +74,34 @@ public class testBulkTransfer
             System.out.print(array[i]);
         }
     }
+
+    @Test
+    public void brokenLinks()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.shopsy.in/");
+        List<WebElement> allLinks = driver.findElements(By.xpath("//a"));
+        int size = allLinks.size();
+        ArrayList<String> links = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+        {
+            URL url = null;
+            int statusCode = 0;
+            String eachLink = allLinks.get(0).getAttribute("href");
+            try
+            {
+                url = new URL(eachLink);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if (statusCode > 400)
+                {
+                    links.add(eachLink + " : " + statusCode);
+                }
+            } catch (Exception e)
+            {
+                links.add(eachLink + " : " + statusCode);
+            }
+        }
+        System.out.println(links);
+    }
 }
